3123c177f0a716e5346aefcdf43953b25854aee3
Leonardo Mesquita <mrbolha@gmail.com>
2006-12-07 23:26:58 +0000

    Added:    * GCJInstantiator

--- tck/src/org/objenesis/tck/Main.java
+++ tck/src/org/objenesis/tck/Main.java
@@ -21,6 +21,7 @@ public class Main {
         attemptToRegisterInstantiator(tck, "org.objenesis.SunReflectionFactoryInstantiator", "SunReflection");
         attemptToRegisterInstantiator(tck, "org.objenesis.SunReflectionFactorySerializationInstantiator", "SunReflection for Serialization");
         attemptToRegisterInstantiator(tck, "org.objenesis.Sun13Instantiator", "Sun JDK 1.3");
+        attemptToRegisterInstantiator(tck, "org.objenesis.GCJInstantiator", "GCJ");
 
         CandidateLoader candidateLoader = new CandidateLoader(
                 tck,
