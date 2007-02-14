931d17b81f20fbdb5f805e98164cd9d4f8e9b736
Henri Tremblay <henri.tremblay@gmail.com>
2007-02-14 23:42:27 +0000

    Tuning the release 1.0

--- build_release.bat
+++ build_release.bat
@@ -2,14 +2,23 @@
 
 echo ==================================== Clean ===========================================
 call %M2_HOME%\bin\mvn.bat clean
+cd website
+call %M2_HOME%\bin\mvn.bat clean
+cd ..
 
 echo ==================================== Build ===========================================
 call build_tck.bat
+
+echo ==================================== Bundle ==========================================
+cd main
+call build_release.bat
+cd ..
 
 echo ==================================== Site ============================================
 cd website
 call build.bat
 cd ..
+xcopy /I /E main\target\apidocs website\target\xsite\apidocs
 
 echo ==================================== Test ============================================
 cd target
