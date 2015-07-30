73f014ad5e300bb04ac9a0723267e9f51ffd212e
Henri Tremblay <henri.tremblay@gmail.com>
2015-07-30 00:55:33 -0400

    Bad packaging

--- gae/src/main/java/org/objenesis/gae/JspWriterListener.java
+++ gae/src/main/java/org/objenesis/gae/JspWriterListener.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package main.java.org.objenesis.gae;
+package org.objenesis.gae;
 
 import org.objenesis.tck.search.SearchWorkingInstantiatorListener;
 
