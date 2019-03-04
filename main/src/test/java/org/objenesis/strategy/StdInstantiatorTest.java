42933eeba251334086900061923f52810d495265
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-03-03 20:07:01 -0800

    Debug failing test

--- main/src/test/java/org/objenesis/strategy/StdInstantiatorTest.java
+++ main/src/test/java/org/objenesis/strategy/StdInstantiatorTest.java
@@ -1,14 +1,17 @@
 package org.objenesis.strategy;
 
 import java.util.AbstractList;
+import org.junit.Assume;
 import org.junit.Test;
+import org.objenesis.instantiator.ObjectInstantiator;
 
 public class StdInstantiatorTest {
    @Test
    public void testAbstract() {
       // Can't run on Android until https://github.com/easymock/objenesis/issues/68 is fixed
-      if(!PlatformDescription.isThisJVM(PlatformDescription.DALVIK)) {
-         new StdInstantiatorStrategy().newInstantiatorOf(AbstractList.class).newInstance();
-      }
+      Assume.assumeFalse(PlatformDescription.isThisJVM(PlatformDescription.DALVIK));
+      ObjectInstantiator<AbstractList> instantiator =
+         new StdInstantiatorStrategy().newInstantiatorOf(AbstractList.class);
+      instantiator.newInstance();
    }
 }
