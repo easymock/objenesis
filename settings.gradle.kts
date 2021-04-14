fde7041fbd6baaa18338893f2c7e760b1c00aae3
Michael Bailey <yogurtearl@gmail.com>
2021-04-13 19:35:05 -0700

    Initial Gradle setup
    To build and run non-android tests:
    ./gradlew build
    
    To run the Android TCK test:
    ./gradlew connectedAndroidTest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
    resolutionStrategy {
       eachPlugin {
          if(requested.id.namespace == "com.android"){
-            useModule("com.android.tools.build:gradle:4.1.1")
+            useModule("com.android.tools.build:gradle:4.1.3")
          }
       }
    }
