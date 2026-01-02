dcff373ee0c5e55bdf7a74c38867bba9dcf5f8fa
Henri Tremblay <henri.tremblay@gmail.com>
2026-01-01 23:17:46 -0500

    Update copyrights to 2026

--- module-test/src/main/java/module-info.java
+++ module-test/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2024 the original author or authors.
+ * Copyright 2006-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 module org.mymodule {
    requires org.objenesis;
    exports org.mymodule;
