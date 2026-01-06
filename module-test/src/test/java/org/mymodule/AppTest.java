f9e9770ebf9471020e255fdf6eeaddd35223b038
Henri Tremblay <henri.tremblay@gmail.com>
2026-01-05 21:37:30 -0500

    AppTest should be in a package

--- module-test/src/test/java/org/mymodule/AppTest.java
+++ module-test/src/test/java/org/mymodule/AppTest.java
@@ -13,14 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.mymodule;
+
 import org.junit.jupiter.api.Test;
-import org.mymodule.App;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
 
-   App app = new App();
+   private final App app = new App();
 
    @Test
    void testObject() {
